593c37751ec6d572a68e2746c1cb222aa04e68a4
Bernardo-07 <bernardoduarte271@gmail.com>
2024-01-26 09:03:36 -0300

    Coordinates of points A and B

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#medidas da biela e da manivela em centímetros
 manivela = 16 
 biela = 92
-#milimetros
 
-teta = np.arange(0, 181, 20)
+#cálculo dos angulos
+teta = np.arange(0, 360, 1)
+teta = np.radians(teta)
+beta = np.arcsin((manivela*np.sin(teta))/biela)
 
-#beta = np.arcsin((manivela*np.sin(teta))/biela)
+#cálculo das coordenadas dos pontos A e B
+xa = [None] * len(teta)
+ya = [None] * len(teta)
+xb = [None] * len(teta)
+yb = [None] * len(teta)
+for i in range(0, len(teta)):
+    xa = manivela*np.cos(teta)
+    ya = manivela*np.sin(teta)
+    xb = (biela*np.cos(beta)) + xa
+    yb[i] = 0
+
+plt.scatter(xa, ya)
+plt.scatter(xb, yb)
+plt.show()
